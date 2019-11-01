63730f92074a6c1637fbf948b1c9cda8fd82ca68
dipadana <putudipadana@gmail.com>
2019-11-01 13:56:27 +0700

    update bug

--- routers/index.js
+++ routers/index.js
@@ -3,14 +3,10 @@ const YoutubeController = require('../controllers/YoutubeController')
 const userController = require('../controllers/userController')
 const loginGoogle = require('../middlewares/googleLogin')
 
-
 router.post('/', YoutubeController.findByEmotion)
 router.get('/:movieId', YoutubeController.searchByKeyWords)
 router.post('/register',userController.register)
 router.post('/login',userController.login)
 router.post('/login-google', loginGoogle, userController.loginGoogle)
-
-
-
 
 module.exports = router
